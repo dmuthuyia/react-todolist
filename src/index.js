4954a54b222e671718e8cc59c7b96e678c0eeb77
dmuthuyia <dmuthuyia@gmail.com>
2019-04-10 18:53:07 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+
+var destination = document.querySelector("#container");
+
+ReactDom.render(
+  <div>
+    <p>Hello</p>
+  </div>,
+  destination
+);
